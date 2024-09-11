5b04a67dc587af15f8ca1d46eb05cf8f9a7bbe07
franfreezy <frandelwanjawa@yahoo.com>
2024-09-11 09:26:13 +0300

    Fixing allowed hosts

--- FreezyBackend/FreezyBackend/settings.py
+++ FreezyBackend/FreezyBackend/settings.py
@@ -118,6 +118,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://devfreezy.netlify.app/"
+    "https://devfreezy.netlify.app"
     
 ]
