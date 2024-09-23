16e7d56fd10925ac23ee59dc1da208021e63cad2
franfreezy <frandelwanjawa@yahoo.com>
2024-09-23 19:05:32 +0300

    Daraja devs in the mud

--- FreezyBackend/backend/views.py
+++ FreezyBackend/backend/views.py
@@ -12,20 +12,4 @@ def api_data(request):
     }
     return JsonResponse(data)
 
-from django_daraja.mpesa.core import MpesaClient
 
-def index(request):
-    cl = MpesaClient()
-   
-    phone_number = '0729634366'
-    amount = 1
-    account_reference = 'reference'
-    transaction_desc = 'Description'
-    callback_url = 'https://devfreezy.netlify.app'
-    response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
-    return HttpResponse(response)
-
-def stk_push_callback(request):
-        data = request.body
-        
-        return HttpResponse("STK Push in Django  by Freezy")
